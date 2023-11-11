0c6e94cd6469fbc6777d0e2a4cce83a540b49a52
GabOl-code <73078610+GabOl-code@users.noreply.github.com>
2023-11-11 12:43:39 +0100

    L_Commit1

---
+++ input.py
@@ -0,0 +1,12 @@
+inputresult1 = input ("Írj be egy szöveget: ")
+
+print(inputresult1)
+print(type(inputresult1))
+
+inputresult2 = input ("Írj be egy számot: ")
+
+if not isinstance(inputresult2, int):
+    raise TypeError("Nem szám")
+else:
+    if inputresult2 < 10:
+        raise ValueError("10-nél nagyobb számot adj meg!")
